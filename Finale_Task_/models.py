8a2e008afc958c3c4b1681693309d03eb600f32d
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-10-13 17:48:12 +0100

    verified

---
+++ Finale_Task_/models.py
@@ -0,0 +1,37 @@
+from app import db
+
+# table User
+class User(db.Model):
+    """Model representing a user in the system."""
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(10), default='user')# user/admin
+
+# table Product for e-commerce (Database Design part)
+class Product(db.Model):
+    """Model representing a product in the system."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+# table Order
+class Order(db.Model):
+    """Model representing an order in the system."""
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    total_amount = db.Column(db.Float, nullable=False)
+    user = db.relationship('User', backref=db.backref('orders', lazy=True))
+
+# table OrderItem
+class OrderItem(db.Model):
+    """Model representing an order item in the system."""
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    order = db.relationship('Order', backref=db.backref('order_items', lazy=True))
+    product = db.relationship('Product', backref=db.backref('order_items', lazy=True))
